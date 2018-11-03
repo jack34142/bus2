e5bd6fe1b1663c63cb05206d2fd5a090484f3cde
Jack <jack34142@gmail.com>
2018-11-04 02:40:07 +0800

    optimize Thread and debug the problem of the map is null

--- app/src/main/java/com/imge/bus2/MainActivity.java
+++ app/src/main/java/com/imge/bus2/MainActivity.java
@@ -1,10 +1,13 @@
 package com.imge.bus2;
 
+import android.app.Dialog;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.WindowManager;
 
 import com.google.android.gms.maps.SupportMapFragment;
 import com.imge.bus2.model.MyFileIO;
@@ -19,8 +22,9 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     DataDownload dataDownload;
-    public static Object downloadObj = new Object();
     public static Handler handler;
+    private Dialog dialog_wait;
+    public static Thread downloadThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         dataDownload = new DataDownload(MainActivity.this);
         setMap();
         setHandler();
-        setDownloadThread();
+        checkDownload();
     }
 
     @Override
@@ -40,40 +44,45 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // 檢查有沒有下載過必要資料
-    private void setDownloadThread(){
+    private void checkDownload(){
 
-            new Thread(new Runnable() {
+        // 檢查有沒有下載過必要資料
+        if( !MyLog.getIsDownload(MainActivity.this) ){
+            downloadThread = new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    // 檢查有沒有下載過必要資料
-                    if(!MyLog.getIsDownload(MainActivity.this)){
+                    // 如果沒有，先下載各路線中文名
+                    dataDownload.getRouteName();
 
-                        dataDownload.getRouteName();        // 如果沒有，先下載各路線中文名
-                        synchronized (downloadObj) {
-                            try {
-                                downloadObj.wait();
-                            } catch (Exception e) {
-                                Log.e("MainActivity", "getRouteName() wait() 出錯");
-                                e.printStackTrace();
-                            }
-                        }
+                    try{
+                        Thread.sleep(Long.MAX_VALUE);
+                    }catch (Exception e){}
 
-                        dataDownload.getBusStops();     // 再來下載站點詳細資料
-                        synchronized (downloadObj) {
-                            try {
-                                downloadObj.wait();
-                            } catch (Exception e) {
-                                Log.e("MainActivity", "getBusStops() wait() 出錯");
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-
-                    handler.sendEmptyMessage(0);
-
+                    // 再來下載站點詳細資料
+                    dataDownload.getBusStops();
                 }
-            }).start();
+            });
+            downloadThread.start();
+        }else{
+            // 如果下載過，那就直接 setMark()
+            // 否則會在 dataDownload.getBusStops() 後執行
+            setMark();
+        }
     }
+
+    private void setDialog(){
+        dialog_wait = new Dialog(MainActivity.this);
+        dialog_wait.setContentView(R.layout.dialog_wait);
+        dialog_wait.setCancelable(false);
+
+        // 螢幕大小 .d.heightPixels = 高 , d.widthPixels = 寬
+        DisplayMetrics d = getResources().getDisplayMetrics();
+
+        WindowManager.LayoutParams p = new WindowManager.LayoutParams();
+        p.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        p.width = (int) (d.widthPixels * 0.85);
+        dialog_wait.getWindow().setAttributes(p);
+    };
 
     // 啟用 map
     private void setMap(){
@@ -86,15 +95,7 @@ public class MainActivity extends AppCompatActivity {
     private void setMark(){
         BusStopDAO busStopDAO = new BusStopDAO(MainActivity.this);
         Map<String, List> map = busStopDAO.getAll();
-        List value;
-        for(String stopName : map.keySet()){
-            value = map.get(stopName);
-            MapTools.getInstance().setMark(
-                    stopName,
-                    (Double) value.get(1),
-                    (Double) value.get(2)
-            );
-        }
+        MapTools.getInstance().checkMapReady(MainActivity.this, map);
     }
 
     private void setHandler(){
@@ -104,6 +105,7 @@ public class MainActivity extends AppCompatActivity {
                 super.handleMessage(msg);
                 switch (msg.what){
                     case 0:
+                        downloadThread = null;
                         setMark();
                         break;
                     default:
--- app/src/main/java/com/imge/bus2/myTools/MapTools.java
+++ app/src/main/java/com/imge/bus2/myTools/MapTools.java
@@ -1,14 +1,23 @@
 package com.imge.bus2.myTools;
 
+import android.app.Activity;
+import android.content.Context;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.imge.bus2.MainActivity;
+import com.imge.bus2.mySQLite.BusStopDAO;
+
+import java.util.List;
+import java.util.Map;
 
 public class MapTools implements OnMapReadyCallback {
     private static MapTools instance;
     GoogleMap mMap;
+    private Thread cheackThread;
 
     private MapTools() {
         super();
@@ -31,6 +40,42 @@ public class MapTools implements OnMapReadyCallback {
         LatLng sydney = new LatLng(25.034419, 121.302718);
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 17.0f));
+
+        if(cheackThread != null){
+            cheackThread.interrupt();
+        }
+    }
+
+    public void checkMapReady(final Activity activity, final Map<String, List> map){
+
+            cheackThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    if(mMap == null){
+                        try {
+                            Thread.sleep(Long.MAX_VALUE);
+                        } catch (Exception e) {}
+                    }
+
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            List value;
+                            for (String stopName : map.keySet()) {
+                                value = map.get(stopName);
+
+                                MapTools.getInstance().setMark(
+                                        stopName,
+                                        (Double) value.get(1),
+                                        (Double) value.get(2)
+                                );
+                            }
+                        }
+                    });
+
+                }
+            });
+            cheackThread.start();
     }
 
     public void setMark(String stopName, double latitude, double longitude){
